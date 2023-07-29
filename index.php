fcbc88b9e9c82705d077f64e1b1b36a84ab8f755
AmiraTarek99 <amiratarek1410@gmail.com>
2023-07-29 11:22:11 +0300

    index file

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php 
+
+$servername = "MYSQL5047.site4now.net";
+$username = "a82e8b_a3go";
+$password = "vAxMvxqtTXAw8Vq";
+$db="db_a82e8b_a3go";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password,$db);
+
+if ($conn->connect_error) {
+	die("Connection failed: " . $conn->connect_error);
+  }
+
+  <>
+
+?>
+
